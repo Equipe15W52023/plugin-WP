1f685dc470491d684d404d3240e865c2aaed43d8
Michelle <e2078172@cmaisonneuve.qc.ca>
2023-11-22 11:31:49 -0500

    programmation javascript et php du carrousel d'articles

--- carrousel.php
+++ carrousel.php
@@ -3,53 +3,84 @@
  * Plugin name: Carrousel
  * Author: Michelle Salois Gadoury
  * Description: Cette extension permettra d'afficher plusieurs vidéos de projets 
- * Version: 1.1.0
+ * Version: 1.2.0
  */
 
-function creation_carrousel() {
+function creation_carrousel($atts) {
+    ob_start();
     
-    $leCarrousel = '<div class="carrousel">';
+    //$leCarrousel = '<div class="article">';
+      //$leCarrousel .= '</div>';
+           /* $leCarrousel .= '<div class="carrousel__bouton">';
+                    $leCarrousel .= '<button id="carrousel__gauche"><--</button>';
+                    $leCarrousel .= '<button id="carrousel__droite">--></button>';
+                    $leCarrousel .= '</div>';*/
+     $atts = shortcode_atts(array(
+        'posts_per_page' => 5, // Nombre d'articles à afficher
+    ), $atts);               
     
-        $args = array(
+        $query_args = array(
             'post_type' => 'post',
             'category_name' => 'projet',
-            'posts_per_page' => -1,
+            'posts_per_page' => $atts['posts_per_page'],
         );
-         $query = new WP_Query($args);
+         $query = new WP_Query($query_args);
 
         if ($query->have_posts()) {
-            $leCarrousel .= '<div class="carrousel_projets">';
+            echo '<div id="carousel">';
+            //$leCarrousel .= '<div class="tructruc">';
            
-            while ($query->have_posts()) : $query->the_post();
+            while ($query->have_posts()) {
+            $query->the_post();
+        echo '<div class="tructruc">';
+            echo '<div>' . get_the_title() . '</div>';
+            echo '<div>' . get_the_content() . '</div>';
+             echo '</div>';
+            }
+        echo '</div>';
 
-                $projet_title = get_the_title();
-                $projet_content = get_the_content();
-                $projet_video = get_post_gallery();
+                //$projet_title = get_the_title();
+                //$projet_content = get_the_content();
+               // $projet_video = get_post_gallery();
 
-                    $leCarrousel .= '<div class="video_projet">';
-                    $leCarrousel .= '<div class="la_video">' . $projet_video . '</div>';
+       
+        //$leCarrousel .= '<div class="article">';
+       // $leCarrousel .= '<h2 class="titre_projet">' . $projet_title . '</h2>';
+        //$leCarrousel .= '<p class="descritpion_projet">' . $projet_content . '</p>';
+
+                  /* $leCarrousel .= '<div class="video_projet">';
+                   // $leCarrousel .= '<div class="la_video">' . $projet_video . '</div>';
                     $leCarrousel .= '</div>';
 
                     $leCarrousel .= '<div class="info_projet">';
                     $leCarrousel .= '<h3 class="titre_projet">' . $projet_title . '</h3>';
-                    //$leCarrousel .= '<p class="descritpion_projet">' . $projet_content . '</p>';
+                    $leCarrousel .= '<p class="descritpion_projet">' . $projet_content . '</p>';*/
+
                     
+//$leCarrousel .= '</div>';
+          
 
-                    $leCarrousel .= '<div class="carrousel__bouton">';
-                    $leCarrousel .= '<button id="carrousel__gauche"><--</button>';
-                    $leCarrousel .= '<button id="carrousel__droite">--></button>';
-                    $leCarrousel .= '</div>';
-                    $leCarrousel .= '</div>';
+            //endwhile;    
+        //}
 
-            $leCarrousel .= '</div>';
+     echo '<button id="prevBtn" onclick="plusSlides(-1)">Précédent</button>';
+        echo '<button id="nextBtn" onclick="plusSlides(1)">Suivant</button>';
+    }
 
-            endwhile;    
-        }
+    wp_reset_postdata();
 
-    $leCarrousel .= '</div>';
-
-    return $leCarrousel;
+    $output = ob_get_clean();
+    return $output;
 }
+
+
+   // $leCarrousel .= '</div>';
+
+    //return $leCarrousel;
+    //wp_reset_postdata();
+    //return ob_get_clean();
+//}
+
 
 function mon_enqueue_js(){
     $version_js = filemtime(plugin_dir_path(__FILE__) . "js/carrousel.js");    
@@ -63,3 +94,4 @@ function mon_enqueue_js(){
 
 add_action('wp_enqueue_scripts', 'mon_enqueue_js');
 add_shortcode('carrousel', 'creation_carrousel');
+?>
--- js/carrousel.js
+++ js/carrousel.js
@@ -1,167 +1,28 @@
-/*(function(){
-   //console.log('Début du carrousel')
-   let carrousel__ouvrir = document.querySelector('.carrousel__ouvrir')
-   let carrousel = document.querySelector('.carrousel')
-   //let carrousel__x = document.querySelector('.carrousel__x')
-   let carrousel__figure = document.querySelector('.carrousel__figure')
-   let carrousel__form = document.querySelector('.carrousel__form')
-   let carrousel__gauche = document.querySelector('.carrousel__gauche')//!!
-   let carrousel__droite = document.querySelector('.carrousel__droite')//!!
-    let video_du_projet = document.querySelector('.video_projet')
-   //console.log(carrousel__form.tagName) //conteneur de radio-boutons
+let slideIndex = 1;
 
-   let galerie = document.querySelector('.galerie', )
-   let galerie__img = galerie.querySelectorAll('img')
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("tructruc");
 
-  /* PAS BESOIN D'UN POP POP
-   galerie.addEventListener('mousedown', function(){
-         carrousel.classList.add('carrousel--activer')
-     })
-
-   carrousel__x.addEventListener('mousedown', function(){
-      carrousel.classList.remove('carrousel--activer')
-   })*/
-
-/**
- * Pour chaque image de la galerie l'ajouter dans le carrousel
- */
-/*let position = 0
-let index = 0
-let ancienIndex = -1
-
-   /*for (const elem of galerie__img){
-
-      elem.dataset.index = position
-
-      elem.addEventListener('mousedown',function(){
-      index = this.dataset.index
-      affiche_image_carrousel(index)
-      console.log(index)
-    })
-
-      
-   }*/
-/*ajouter_une_image_dans_carrousel()
-ajouter_des_boutons_dans_le_carrousel()
-affiche_image_carrousel();
-
-/**
- * Création dynamique d'une image pour le carrousel
- * @param {*} elem une image de la galerie
- */
-/*function ajouter_une_image_dans_carrousel()
-{
-   //let video = document.createElement('.video_projet')
-   video_du_projet.classList.add('carrousel__img')
-   video_du_projet.src = elem.src
-   // console.log(img.src)
-   carrousel__figure.append(video_du_projet);
-}
-
-/*function ajouter_des_boutons_dans_le_carrousel(){
-   //let bou = document.createElement('input')
-   //bou.setAttribute('type', 'button')
-   //bou.setAttribute('name', 'carrousel__bou')
-   carrousel__gauche.addEventListener('mousedown', function(){
-      //carrousel.classList.add('carrousel__moins')
-      index = 3
-      console.log(index)
-  })
-  carrousel__droite.addEventListener('mousedown', function(){
-   carrousel.classList.add('carrousel__plus')
-
-})*/
-   /*bou.classList.add('carrousel__bou')
-   bou.dataset.index = position
-   bou.addEventListener('mousedown', function(){
-      position = position + 1
-      index = this.dataset.index
-      affiche_image_carrousel();
-   })*/
-   //position = position + 1 //incrémentation de la position
-   //carrousel__form.appendChild(bou);
-//}
-
-/**
- * Affiche la nouvelle image du carrouel
- */
-/*function affiche_image_carrousel(){
-   if(ancienIndex != -1){
-      carrousel__figure.children[ancienIndex].style.opacity = "0"
-      //carrousel__figure.children[ancienIndex].classList.remove('carrousel__img--activer');
-   }
-   //console.log(this.dataset.index)
-   carrousel__figure.children[index].style.opacity = "1"
-   //carrousel__figure.children[index].classList.add('carrousel__img--activer');
-   ancienIndex = index
-}*/
-
-
-//})
-
-
-/*document.addEventListener('DOMContentLoaded', function() {
-    var boutonChanger = document.getElementById('carrousel__gauche');
-    var articleContent = document.querySelector('.carrousel'); // ou sélectionnez votre élément article spécifique
-
-    boutonChanger.addEventListener('click', function() {
-        // Modifiez le contenu de l'article comme vous le souhaitez
-        articleContent.innerHTML = '<p>Nouveau contenu de l\'article.</p>';
-        console.log('coucou');
-       // in_category('projet')s
-    });
-});*/
-
-/*document.addEventListener('DOMContentLoaded', function () {
-    // Sélectionnez les éléments nécessaires
-    //var sessionButtons = document.querySelectorAll('.session-button');
-    var sessions = document.querySelectorAll('.carrousel_projets');
-    var boutonChanger = document.getElementById('.carrousel__gauche');
-
-    // Fonction pour masquer toutes les sessions
-    function hideAllSessions() {
-        sessions.forEach(function (session) {
-            session.style.display = 'none';
-        });
+    if (n > slides.length) {
+        slideIndex = 1;
     }
 
-    // Par défaut, masquez toutes les sessions sauf la première
-    hideAllSessions();
-    sessions[0].style.display = 'flex';
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
 
-    // Ajoutez des gestionnaires d'événements aux boutons
-    boutonChanger.forEach(function (button) {
-        button.addEventListener('click', function () {
-            // Obtenez le numéro de session à afficher à partir de l'attribut "data-session"
-            var sessionNumber = button.getAttribute('data-session');
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
 
-            // Masquez toutes les sessions
-            hideAllSessions();
+    slides[slideIndex - 1].style.display = "block";
+}
 
-            // Affichez la session correspondante
-            var sessionToShow = document.querySelector('.session:nth-child(' + sessionNumber + ')');
-            sessionToShow.style.display = 'flex';
-        });
-    });
-});*/
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
 
 document.addEventListener('DOMContentLoaded', function () {
-    // Récupérer l'ID de la catégorie actuelle
-    var currentCategoryId = document.querySelector('projet').getAttribute("9");
-
-    // Écouter le clic sur le bouton précédent
-    document.getElementById('carrousel__gauche').addEventListener('click', function () {
-        loadArticleByCategory(currentCategoryId, 'precedent');
-    });
-
-    // Écouter le clic sur le bouton suivant
-    document.getElementById('carrousel__droite').addEventListener('click', function () {
-        loadArticleByCategory(currentCategoryId, 'suivant');
-    });
+    showSlides(slideIndex);
 });
-
-function loadArticleByCategory(categoryId, direction) {
-    // Utiliser l'API REST de WordPress pour récupérer les articles de la même catégorie
-    // et gérer la logique pour obtenir l'article précédent ou suivant en fonction de la direction
-    // Mettez à jour le contenu de la page avec le nouvel article
-}
