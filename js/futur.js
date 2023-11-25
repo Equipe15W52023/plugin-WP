ba1fb1773933ff56d77657e2445a7f4b8219dc16
SharlyMarcil <sharly.marcil@gmail.com>
2023-11-25 18:13:13 -0500

    Suppression des console log

--- js/futur.js
+++ js/futur.js
@@ -6,7 +6,6 @@ document.addEventListener('DOMContentLoaded', function () {
     // Ajouter des gestionnaires d'événements aux sections 'futur' pour afficher le contenu
     sectionsFutur.forEach(function (section) {
         section.addEventListener('mousedown', function () {
-           console.log('X_X');
             // Si la section n'est pas affiché, on l'affiche 
             if(!section.classList.contains('texte-futur-selection')) {
                 // Faire apparaitre le contenu des cours en changeant les classes de l'élément
@@ -17,7 +16,6 @@ document.addEventListener('DOMContentLoaded', function () {
                 // Enlever l'affichage du contenu séléctionné
                 var boutonX = document.querySelector('.texte-futur-selection .bouton-x-futur');
                 boutonX.addEventListener('click', function () {
-                    console.log('Xx_xX');
                     desactiverSection();
                 });
             }
@@ -29,7 +27,6 @@ document.addEventListener('DOMContentLoaded', function () {
     // Fonction pour enlever la classe 'cours-selection' des cours non séléctionné
     function desactiverSection() {
         sectionsFutur.forEach(function (section) {
-            console.log('-_-');
             section.classList.add('section-ferme');
             section.classList.remove('texte-futur-selection');
         });
