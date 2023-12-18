71819e51a57b337cb19019793c9694395d032ef6
SharlyMarcil <sharly.marcil@gmail.com>
2023-12-17 23:33:44 -0500

    Animation futur

--- js/futur.js
+++ js/futur.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
     // Sélectionnez les éléments nécessaires
     var sectionsFutur = document.querySelectorAll('.section-futur');   
+    var textesFutur = document.querySelectorAll('.texte-futur'); 
       
     // var corps = document.querySelector('.home');   
 
@@ -9,17 +10,24 @@ document.addEventListener('DOMContentLoaded', function () {
     // Ajouter des gestionnaires d'événements aux sections 'futur' pour afficher le contenu
     sectionsFutur.forEach(function (section) {
         section.addEventListener('mousedown', function () {
-            var introFutur = document.querySelector('.desc-futur'); 
-            var introTitreFutur = document.querySelector('.section-futur h2'); 
-
+            
             // Si la section n'est pas affiché, on l'affiche 
             if(!section.classList.contains('texte-futur-selection')) {
                 // Faire apparaitre le contenu des cours en changeant les classes de l'élément
                 desactiverSection();
                 section.classList.toggle('texte-futur-selection');
                 section.classList.toggle('section-ferme');
-                introFutur.classList.toggle('intro-ferme');
-                introTitreFutur.classList.toggle('intro-ferme');
+
+                // Appeler l'animation d'affichage
+                textesFutur.forEach(function (texteFutur) {
+                    if(texteFutur.classList.contains('agrandissement')) {
+                        texteFutur.classList.remove('agrandissement');
+                        texteFutur.classList.add('agrandissement');
+                    } else {
+                        texteFutur.classList.add('agrandissement');
+                    }
+                });
+                
             } else { // si la section est affichée
                 // Enlever l'affichage du contenu séléctionné
                 section.classList.toggle('texte-futur-selection');
@@ -29,12 +37,6 @@ document.addEventListener('DOMContentLoaded', function () {
             }
         });
     });
-
-    // corps.addEventListener('mousedown', function () {
-    //     desactiverSection();
-    //     console.log('x_X)');
-    // });
- 
     
 
     // FONCTIONS
